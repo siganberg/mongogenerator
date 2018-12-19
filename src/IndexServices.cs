0794618ff4f115978ba58dc8c1f491897924c148
francis@marasigan.net <francis@marasigan.net>
2018-12-18 20:53:15 -0800

    - first commit.

---
+++ src/IndexServices.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace MongoGenerator
+{
+    public class IndexEntity
+    {
+        public int Version { get; set; }
+        public BsonDocument Key { get; set; }
+        public string Name { get; set; }
+        public string CollectionName { get; set; }
+        public bool Background { get; set; }
+    }
+
+    public class IndexKeyEntity
+    {
+        public string Name { get; set; }
+
+    }
+
+    public interface IIndexServices
+    {
+        void Generate();
+    }
+
+    public class TestDbMongClient : MongoClient
+    {
+        private readonly IConfiguration _config; 
+        public TestDbMongClient(IConfiguration config) : base(config.GetConnectionString("MongoDB"))
+        {
+
+        }
+    }
+
+
+    public class IndexServices : IIndexServices
+    {
+        private readonly IMongoClient _client;
+        private readonly ILogger<IndexServices> _logger;
+        public IndexServices(IMongoClient client, ILogger<IndexServices> logger)
+        {
+            _client = client;
+            _logger = logger;
+            RegisterMapping();
+        }
+
+        private void RegisterMapping()
+        {
+            BsonClassMap.RegisterClassMap<IndexEntity>(cm =>
+            {
+                cm.MapMember(c => c.Version).SetElementName("v");
+                cm.MapMember(c => c.Key).SetElementName("key");
+                cm.MapMember(c => c.CollectionName).SetElementName("ns");
+                cm.MapMember(c => c.Name).SetElementName("name");
+                cm.MapMember(c => c.Background).SetElementName("background");
+            });
+        }
+
+        public void Generate()
+        {
+            var db = _client.GetDatabase("ugc");
+            
+            foreach (var c in db.ListCollectionNamesAsync().Result.ToListAsync().Result)
+            {
+                var docs = db.GetCollection<BsonDocument>(c).Indexes.ListAsync().Result.ToListAsync().Result;
+                var indexes = docs.Select(a => BsonSerializer.Deserialize<IndexEntity>(a));
+
+                _logger.LogInformation($"//-- {c} collection");
+                foreach(var index in indexes.Where(a => a.Name != "_id_"))
+                    _logger.LogInformation($"db.{c}.createIndex({{{index.Key}}}, {{ background:true }});");
+            }
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Serilog;
+
+namespace MongoGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IConfiguration>(a => CreateConfiguration())
+                .AddSingleton<IMongoClient, TestDbMongClient>()
+                .AddSingleton<IIndexServices, IndexServices>()
+                .AddLogging()
+                .BuildServiceProvider();
+
+            ConfigureLogger(serviceProvider);
+
+            var indexer = serviceProvider.GetService<IIndexServices>();
+            indexer.Generate();
+
+            Console.ReadLine();
+
+        }
+
+        private static IConfigurationRoot CreateConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json", false, true);
+
+            var config = builder.Build();
+            return config;
+        }
+
+        private static void ConfigureLogger(ServiceProvider serviceProvider)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.LiterateConsole()
+                .CreateLogger();
+
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+            loggerFactory.AddSerilog();
+        }
+    }
+}
